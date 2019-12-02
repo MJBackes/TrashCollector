f8cc20b42352f2bc80911f2e26506de99e29b841
MJBackes <mjohnbackes@gmail.com>
2019-12-02 14:30:21 -0600

    Implemented Google Maps api to show the customer's address as a pin on a map when in the employees CustomerMap view.

---
+++ TrashCollector/App_Start/URLFormatter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace TrashCollector.App_Start
+{
+    public static class URLFormatter
+    {
+        public static string Format(string input)
+        {
+            StringBuilder stringBuilder = new StringBuilder("");
+            foreach (char c in input.ToCharArray())
+                if (c != ' ')
+                    stringBuilder.Append(c);
+                else
+                    stringBuilder.Append('+');
+            return stringBuilder.ToString();
+        }
+    }
+}
--- TrashCollector/Models/Customer.cs
+++ TrashCollector/Models/Customer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Text;
 using System.Web;
 
 namespace TrashCollector.Models
