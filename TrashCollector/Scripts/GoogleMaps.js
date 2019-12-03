0c5f698e0f872f22d1d853458614aea139de4b4a
MJBackes <mjohnbackes@gmail.com>
2019-12-03 14:42:42 -0600

    Added a function to the googlemaps script to place markers at the addresses of customers. Also broke code snippets out of the main initMap function into helper functions for better single responsibility.

--- TrashCollector/Scripts/GoogleMaps.js
+++ TrashCollector/Scripts/GoogleMaps.js
@@ -1,12 +1,39 @@
 ﻿async function initMap() {
+
+    let addresses = getAddresses();
+    let names = getNames();
+    let zipcode = document.getElementsByClassName("zipCol")[0].innerHTML;
+
+    let positions = [];
+    positions = await getPositions(positions, addresses, zipcode);
+
+    let options = await setOptions(zipcode);
+
+    var map = new google.maps.Map(document.getElementById("map"), options);
+    placeMarkers(positions, names, map);
+}
+
+function getAddresses() {
     let addressCols = document.getElementsByClassName("addressCol");
     let addresses = [];
     for (i = 0; i < addressCols.length; i++)
         addresses.push(addressCols[i].innerHTML);
-    let zipcode = document.getElementsByClassName("zipCol")[0].innerHTML;
+    return addresses;
+}
+
+function getNames() {
+    let nameCols = document.getElementsByClassName("nameCol");
+    let names = [];
+    for (let i = 0; i < nameCols.length; i++)
+        names.push(nameCols[i].innerHTML.split('>')[1].split('<')[0]);
+    return names;
+}
+
+async function setOptions(zipcode) {
     let lat = 43;
     let long = -88.25;
     let zoom = 9;
+
     let geocoder = new google.maps.Geocoder();
     await geocoder.geocode({ 'address': zipcode }, function (results, status) {
         if (status == google.maps.GeocoderStatus.OK) {
@@ -15,13 +42,35 @@
             zoom = 12;
         }
     });
-    let options = {
+    return {
         zoom: zoom,
-        center: {
-            lat: lat, lng: long
-        }
+        center: { lat: lat, lng: long }
     };
-    let map = new google.maps.Map(document.getElementById("map"), options);
+}
 
+function placeMarkers(positions, names, map) {
+    let markers = []
+    for (let i = 0; i < positions.length; i++) {
+        markers.push(new google.maps.Marker({
+            position: positions[i],
+            map: map,
+            title: names[i]
+        }));
+    }
+}
 
+async function getPositions(positions,addresses,zip) {
+    let lat = 43;
+    let long = -88;
+    let geocoder = new google.maps.Geocoder();
+    for (let i = 0; i < addresses.length; i++) {
+            await geocoder.geocode({ 'address': addresses[i] + " " + zip }, function (results, status) {
+                if (status == google.maps.GeocoderStatus.OK) {
+                    lat = results[0].geometry.location.lat();
+                    long = results[0].geometry.location.lng();
+                }
+            });
+        positions[i] = { lat: lat, lng: long };
+    }
+    return positions;
 }
